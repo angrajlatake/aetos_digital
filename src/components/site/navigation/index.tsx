37cded5090e13455dbd9169ce57386f5cec42b52
angrajlatake <angraj.latake@gmail.com>
2024-02-29 10:57:15 -0500

    first commit

---
+++ src/components/site/navigation/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { User } from "@clerk/nextjs/server";
+type Props = {
+  user?: null | User;
+};
+
+const Navigation = () => {
+  return <div>Navigation</div>;
+};
+
+export default Navigation;
