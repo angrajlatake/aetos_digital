649c1aa3a748afdda84f426892892ab6e1b8107a
angrajlatake <angraj.latake@gmail.com>
2024-02-29 12:02:14 -0500

    added navbar with links and darkmode

--- src/app/site/layout.tsx
+++ src/app/site/layout.tsx
@@ -1,7 +1,13 @@
+import Navigation from "@/components/site/navigation";
 import React from "react";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
-  return <main className="h-full">{children}</main>;
+  return (
+    <main className="h-full">
+      <Navigation />
+      {children}
+    </main>
+  );
 };
 
 export default layout;
--- src/components/site/navigation/index.tsx
+++ src/components/site/navigation/index.tsx
@@ -1,11 +1,52 @@
 import React from "react";
 import { User } from "@clerk/nextjs/server";
+import Image from "next/image";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { ModeToggle } from "@/components/global/mode-toggle";
+
 type Props = {
   user?: null | User;
 };
 
-const Navigation = () => {
-  return <div>Navigation</div>;
+const Navigation = ({ user }) => {
+  return (
+    <div className="p-4 flex justify-between relative">
+      <aside className="flex items-center gap-2">
+        <Image
+          className="transition-all dark:hidden"
+          src="/assets/logo_light.png"
+          alt="logo"
+          width={40}
+          height={40}
+        />
+        <Image
+          src="/assets/logo_dark.png"
+          className="hidden  transition-all dark:block"
+          alt="logo"
+          width={40}
+          height={40}
+        />
+        <span className="text-2xl font-bold">Aetos.</span>
+      </aside>
+      <nav className="hidden md:block absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%]">
+        <ul className="flex items-center justify-center gap-8">
+          <Link href={"#"}>Pricing</Link>
+          <Link href={"#"}>About</Link>
+          <Link href={"#"}>Documentation</Link>
+          <Link href={"#"}>Features</Link>
+        </ul>
+      </nav>
+      <aside className="flex gap-2 items-center">
+        <Link href={"/agency"} className={buttonVariants()}>
+          Login
+        </Link>
+        <UserButton />
+        <ModeToggle />
+      </aside>
+    </div>
+  );
 };
 
 export default Navigation;
